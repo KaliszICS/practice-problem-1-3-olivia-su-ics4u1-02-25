68470e242ab4364417dde3e655e4e0acca1f54c0
olivia-su-ics4u1-02-25 <olivia.su@student.tdsb.on.ca>
2025-03-24 13:07:56 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,68 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
+	public static String readFile(String filename){
+		Scanner sc = null;
+		String theFile = "";
 	
+		try{
+			sc = new Scanner(new File(filename));
+			while (sc.hasNextLine()){
+				theFile += sc.nextLine() + "\n";
+				
+			}
+			return theFile;
 
+		}catch(FileNotFoundException e){
+			return "cannot read";
+
+		}finally {
+			try{
+				if (sc != null){
+				sc.close();
+				}
+			}catch(Exception e){
+				return "cannot close";
+			}
+		}
+
+	}
+
+	public static String backwardsReadFile(String filename){
+		Scanner sc = null;
+		String theRevFile = "";
+
+		try{
+			sc = new Scanner(new File(filename));
+			while(sc.hasNextLine()){
+				String theLine = sc.nextLine();
+				String theRevLine = "";
+
+				for(int i = theLine.length() - 1; i >= 0; i--){
+					theRevLine += theLine.charAt(i);
+				}
+
+				theRevFile = theRevLine + "\n" + theRevFile;
+			}
+			return "\n"+ theRevFile.trim();
+
+		}catch(FileNotFoundException e){
+			return "cannot read";
+		}finally{
+			try{
+				if(sc != null){
+					sc.close();
+				}
+			}catch(Exception e){
+				return "cannot close";
+			}
+		}
+	}
 }
+
